110f23cabbb3cf27c0c1199b42766cdf39e29fe6
dipanshurai.11@gmail.com <dipanshurai.11@gmail.com>
2024-09-27 10:12:54 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,10 +1,7 @@
 module "vpc1" {
   source = "./modules"
 
-<<<<<<< HEAD
   isPrivateSubnetNeeded = true
-=======
->>>>>>> main
   vpc_cidr             = "172.31.0.0/16"
   vpc_instance_tenency = "default"
   public_subnet_az     = "us-east-2a"
@@ -15,7 +12,6 @@ module "vpc1" {
   batch_mode           = "weekday"
   batch_month          = "july"
   provisioned_by       = "Nishant"
-<<<<<<< HEAD
 
   ec2_attributes = {
     instance1 = {
@@ -59,49 +55,6 @@ module "vpc1" {
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
     }]
-=======
->>>>>>> main
 
 }
 
-/*
-module "vpc2" {
-  source = "./modules"
-
-<<<<<<< HEAD
-  isPrivateSubnetNeeded = false
-=======
->>>>>>> main
-  vpc_cidr             = "172.31.0.0/16"
-  vpc_instance_tenency = "default"
-  public_subnet_az     = "us-east-2a"
-  public_subnet_cidr   = "172.31.0.0/24"
-  private_subnet_az    = "us-east-2b"
-  private_subnet_cidr  = "172.31.1.0/24"
-  batch_code           = "tg-devops-0830"
-  batch_mode           = "weekday"
-  batch_month          = "aug"
-  provisioned_by       = "Dipanshu"
-
-}
-
-<<<<<<< HEAD
-
-module "vpc3" {
-  source = "./modules"
-
-  isPrivateSubnetNeeded = false
-  vpc_cidr             = "172.31.0.0/16"
-  vpc_instance_tenency = "default"
-  public_subnet_az     = "us-east-2a"
-  public_subnet_cidr   = "172.31.0.0/24"
-  private_subnet_az    = "us-east-2b"
-  private_subnet_cidr  = "172.31.1.0/24"
-  batch_code           = "tg-devops-0830"
-  batch_mode           = "weekday"
-  batch_month          = "aug"
-  provisioned_by       = "Dipanshu"
-
-}*/
-=======
->>>>>>> main
